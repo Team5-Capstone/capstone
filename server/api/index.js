7308ff183ed193f6c33371b46c1271304ed993c6
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2022-09-15 11:18:35 -0400

    add onSubmit function to Editor component + add 'tests' route in server/api/tests.js file

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 module.exports = router
 
-router.use('/users', require('./users'))
+router.use('/tests', require('./tests'));
+router.use('/users', require('./users'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
