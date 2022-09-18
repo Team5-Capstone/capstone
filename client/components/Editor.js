f7d50a55e0f25b36e459c08bd457da81a94408e3
Bob Z Saludo <bob-zs@users.noreply.github.com>
2022-09-18 19:57:08 -0400

    rm extra 1st line

--- client/components/Editor.js
+++ client/components/Editor.js
@@ -18,8 +18,7 @@ const turnOffCtrlS = () => {
   });
 };
 
-const exampleTestCode = `
-const sum = require('./sum');
+const exampleTestCode = `const sum = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
