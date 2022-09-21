6fcf4d6bea6da2a83b4b609dbb66ef0d575ce8be
Fraih001 <90857197+Fraih001@users.noreply.github.com>
2022-09-20 23:16:14 -0400

    add functionality so that user-created test is deleted from jest file after evaluation

--- helloWorld.test.js
+++ helloWorld.test.js
@@ -1,8 +1,3 @@
-const helloWorld = (str) => {
+function helloWorld(str) {
   return `Hello, ${str}!`;
-};
-describe('helloWorld', () => {
-  test('returns a string "Hello World"', () => {
-    expect(helloWorld('World')).toBe('Hello, World!');
-  });
-});
+}
