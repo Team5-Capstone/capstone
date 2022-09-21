9cd325b11d1a5c0a8be1d4fb7f012bf84ddec47c
Bob Z Saludo <bob-zs@users.noreply.github.com>
2022-09-20 21:20:11 -0400

    eh?

--- client/components/Editor.js
+++ client/components/Editor.js
@@ -88,27 +88,30 @@ export const Editor = (props) => {
   }, [narrative]);
 
   const fetchData = () => {
-    axios
-      .post('/api/tests', {
-        code,
-      })
-      .then((res) => {
-        setResponse(res.data);
-      });
+    axios.post('/api/tests', { code }).then((res) => setResponse(res.data));
   };
+  fetchStuff();
 
-  const onSubmit = () => {
-    fetchData();
-  };
+  const onSubmit = () => fetchData();
 
-  const runTest = () => {
-    axios
-      .get('/api/tests/results', {
-        code,
-      })
-      .then((res) => {
-        setResponse(res.data);
-      });
+  // const templateTest = prompts[0]?.templateTest;
+
+  const getReadOnlyRanges = (editor) => {
+    console.log(editor.doc.line);
+    return [
+      {
+        from: undefined, //same as targetState.doc.line(0).from or 0
+        to: editor.doc.line(2).to,
+      },
+      {
+        from: editor.doc.line(4).from, //same as targetState.doc.line(0).from or 0
+        to: editor.doc.line(5).to,
+      },
+      {
+        from: editor.doc.line(editor.doc.lines).from,
+        to: undefined, // same as targetState.doc.line(targetState.doc.lines).to
+      },
+    ];
   };
 
   useEffect(() => {
@@ -144,12 +147,8 @@ export const Editor = (props) => {
 
   const runTest = () => {
     axios
-      .post('/api/tests/results', {
-        code,
-      })
-      .then((res) => {
-        setResponse(res.data);
-      });
+      .get('/api/tests/results', { code })
+      .then((res) => setResponse(res.data));
   };
 
   return (
