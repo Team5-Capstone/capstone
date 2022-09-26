d5fcc0c6080d0a3952ed483daa0a7ac5333bba1c
Bob Z Saludo <bob-zs@users.noreply.github.com>
2022-09-26 14:51:41 -0400

    SQUASH THIS: render test from index

--- client/components/Tests/index.js
+++ client/components/Tests/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import * as AllTests from './';
+import Test1 from './Test1';
+import Test2 from './Test2';
+import Test3 from './Test3';
+import Test4 from './Test4';
+import Test5 from './Test5';
+import Test6 from './Test6';
+import Test7 from './Test7';
+import Test8 from './Test8';
+import Test9 from './Test9';
+import Test10 from './Test10';
+
+const AllTests = [
+  Test1,
+  Test2,
+  Test3,
+  Test4,
+  Test5,
+  Test6,
+  Test7,
+  Test8,
+  Test9,
+  Test10,
+];
 
 const PaginatedTests = () => {
   const Test1 = AllTests[0];
-  return <Test1></Test1>;
+  return (
+    <>
+      <Test1 />
+    </>
+  );
 };
 
 export default PaginatedTests;
