a337fda4694688ef8b8a12568b0d82e3ac316503
Bob Z Saludo <bob-zs@users.noreply.github.com>
2022-10-19 14:57:25 -0400

    Order Prompts FE & BE

--- server/api/prompts.js
+++ server/api/prompts.js
@@ -6,8 +6,9 @@ module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
-    const prompts = await TestingPrompt.findAll();
-    // console.log(prompts);
+    const prompts = await TestingPrompt.findAll({
+      order: [['orderNum', 'ASC']],
+    });
     res.send(prompts);
   } catch (ex) {
     next(ex);
