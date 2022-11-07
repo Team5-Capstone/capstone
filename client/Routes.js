62182c85afa9490a0eca2b098a53c53c6f651461
Bob Z Saludo <bob-zs@users.noreply.github.com>
2022-11-07 11:31:49 -0500

    Deprecate Former Test Routing

--- client/Routes.js
+++ client/Routes.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter, Route, Switch } from 'react-router-dom';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
 
 import { me } from './store';
-// import Test1 from './components/Tests/Test1';
-// import Test2 from './components/Tests/Test2';
-// import Test3 from './components/Tests/Test3';
-// import Test4 from './components/Tests/Test4';
-// import Test5 from './components/Tests/Test5';
-// import Test6 from './components/Tests/Test6';
-// import Test7 from './components/Tests/Test7';
-// import Test8 from './components/Tests/Test8';
-// import Test9 from './components/Tests/Test9';
-// import Test10 from './components/Tests/Test10';
-import PaginatedTests from './components/Tests';
+import DeprecatedPaginatedTests from './components/Tests/DeprecatedPaginatedTests';
+import Pagination from './components/Tests/Pagination';
 
 /**
  * COMPONENT
@@ -30,28 +21,22 @@ class Routes extends Component {
 
     return (
       <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/jest' exact component={PaginatedTests} />
-            {/* <Route path='/jest2' exact component={Test2} />
-            <Route path='/jest3' exact component={Test3} />
-            <Route path='/jest4' exact component={Test4} />
-            <Route path='/jest5' exact component={Test5} />
-            <Route path='/jest6' exact component={Test6} />
-            <Route path='/jest7' exact component={Test7} />
-            <Route path='/jest8' exact component={Test8} />
-            <Route path='/jest9' exact component={Test9} />
-            <Route path='/jest10' exact component={Test10} /> */}
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={Signup} />
-            <Route path='/jest' exact component={PaginatedTests} />
-          </Switch>
-        )}
+        <Switch>
+          {!isLoggedIn && (
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={Signup} />
+            </Switch>
+          )}
+          <Route exact path='/' component={Home} />
+          <Route exact path='/jest/:promptNum' component={Pagination} />
+          <Redirect exact from='/jest' to='/jest/1' />
+          <Route
+            exact
+            path='/deprecatedJest'
+            component={DeprecatedPaginatedTests}
+          />
+        </Switch>
       </div>
     );
   }
